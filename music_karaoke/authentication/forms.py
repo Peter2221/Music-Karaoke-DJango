57711895c02dfde53392c317e4028391e6d68694
AdrianS-labs <adrian.stepnik1997@gmail.com>
2020-11-27 00:29:33 +0100

    added image uploading for user, improved formatting of login and registration forms

--- music_karaoke/authentication/forms.py
+++ music_karaoke/authentication/forms.py
@@ -2,11 +2,16 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
+from .models import Profile
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField()
-    #avatar_photo = forms.ImageField()
 
     class Meta:
         model = User
         fields = ["username", "email", "first_name", "last_name", "password1", "password2"]
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ["profile_pic"]
--- music_karaoke/music_karaoke/settings.py
+++ music_karaoke/music_karaoke/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os.path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'authentication.apps.AuthenticationConfig'
 ]
 
@@ -121,3 +122,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 LOGOUT_REDIRECT_URL = "/"
+
+CRISPY_TEMPLATE_PACK="bootstrap4"

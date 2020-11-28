b6145a2fbbfd7063784e105a5741cb3dcc4a039e
puscik21 <grzesiekpuscizna@gmail.com>
2020-11-28 14:46:39 +0100

    Added Song, Ranking, UserFavouriteSong to admin page

--- music_karaoke/music_karaoke/settings.py
+++ music_karaoke/music_karaoke/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
-    'authentication.apps.AuthenticationConfig'
+    'authentication.apps.AuthenticationConfig',
+    'songs',
 ]
 
 MIDDLEWARE = [

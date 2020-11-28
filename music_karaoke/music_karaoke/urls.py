f85b394d135b1c003c387ec294687960064c5d4a
heIIoad <adrian.stepnik1997@gmail.com>
2020-11-28 13:25:20 +0100

    implementacja podstawowych plików do footera i navbara

--- music_karaoke/music_karaoke/urls.py
+++ music_karaoke/music_karaoke/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from authentication import views as views_auth
+from songs import views as views_song
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', views_auth.register, name="register"),
     path('', include("django.contrib.auth.urls")),
+    path('', views_song.index, name="index")
 ]

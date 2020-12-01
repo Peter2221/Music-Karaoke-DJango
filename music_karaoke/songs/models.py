e7d205b0f7ce2ca4f436aa1d815fea3100eacbad
Peter2221 <pmbasen@gmail.com>
2020-12-01 08:57:33 +0100

    added html

--- music_karaoke/songs/models.py
+++ music_karaoke/songs/models.py
@@ -1,17 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class Song(models.Model):
     title = models.CharField(max_length=50)
     author = models.CharField(max_length=50)
     genre = models.CharField(max_length=50)
 
-
 class UserFavouriteSong(models.Model):
     id_user = models.ForeignKey(to=User, on_delete=models.CASCADE)
     id_song = models.ForeignKey(to=Song, on_delete=models.CASCADE)
-
 
 class Ranking(models.Model):
     id_user = models.ForeignKey(to=User, on_delete=models.CASCADE)
--- music_karaoke/songs/views.py
+++ music_karaoke/songs/views.py
@@ -3,3 +3,4 @@ from django.shortcuts import render, redirect
 # Create your views here.
 def index(request):
     return render(request, 'songs/index.html')
+

2d34fe3c5c324fd57e0102e8235a8be9996f3c1b
puscik21 <grzesiekpuscizna@gmail.com>
2020-12-09 00:18:00 +0100

    test1

--- music_karaoke/music_karaoke/settings.py
+++ music_karaoke/music_karaoke/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -26,8 +25,7 @@ SECRET_KEY = 'h$naq+r6^$9u*-$e%hkr9p2+e(^e(%&3)imr4=*0g^6@umk-#-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['voice-puscik3.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
@@ -38,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms',
+    # 'crispy_forms',
     'authentication.apps.AuthenticationConfig',
     'songs',
     'sass_processor'
@@ -75,7 +73,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'music_karaoke.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -85,7 +82,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -105,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -119,16 +114,17 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 
-MEDIA_ROOT =  os.path.join(BASE_DIR, '') 
+MEDIA_ROOT = os.path.join(BASE_DIR, '')
 MEDIA_URL = '/'
 
-#MEDIA_IMG_URL = '/media/song_images'
+# MEDIA_IMG_URL = '/media/song_images'
 LOGOUT_REDIRECT_URL = "/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
@@ -140,6 +136,6 @@ STATICFILES_FINDERS = [
     'sass_processor.finders.CssFinder',
 ]
 
-#django SASS
-SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR,'static')
+# django SASS
+SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR, 'static')
 LOGOUT_REDIRECT_URL = "/"

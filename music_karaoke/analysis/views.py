2897fb15cb0569101dc0e176683cb2fc0eb6e5a0
Peter2221 <pmbasen@gmail.com>
2021-01-10 22:30:20 +0100

    Add calculate score

--- music_karaoke/analysis/views.py
+++ music_karaoke/analysis/views.py
@@ -1,68 +1,91 @@
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
-from scipy.io import wavfile
 from django.core.files.storage import default_storage
 from django.core.files.base import ContentFile
-from os import path
 from django.conf import settings
+
+from os import path
 import subprocess
 
-# class VoiceAnalyzer:
-#     def read_signal(self, path):
-#         fs, y = wavfile.read(path)
-#         return fs, y
+from scipy.io import wavfile
+from scipy.signal import butter, lfilter, freqz
+from scipy.signal import stft
+from scipy.fft import fft
+import numpy as np
 
-#     def butter_lowpass(self, cutoff, fs, order=5):
-#         nyq = 0.5 * fs
-#         normal_cutoff = cutoff / nyq
-#         b, a = butter(order, normal_cutoff, btype='low', analog=False)
-#         return b, a
 
-#     def butter_lowpass_filter(self, data, cutoff, fs, order=5):
-#         b, a = self.butter_lowpass(cutoff, fs, order=order)
-#         y = lfilter(b, a, data)
-#         return y
+class VoiceAnalyzer:
+    def read_signal(self, path):
+        fs, y = wavfile.read(path)
+        return fs, y
 
-#     def divide_signal_into_chunks(self, y, chunk_size):
-#         return [y[i : i + chunk_size] for i in range(0, len(y), chunk_size)]
+    def butter_lowpass(self, cutoff, fs, order=5):
+        nyq = 0.5 * fs
+        normal_cutoff = cutoff / nyq
+        b, a = butter(order, normal_cutoff, btype='low', analog=False)
+        return b, a
 
-#     def get_frequencies(self, y, chunk_size):
-#         freqs = []
-#         for y_fourier in y:
-#             y_fourier = fft(y_fourier)
-#             y_fourier = y_fourier[0:int(chunk_size / 2)]
-#             # index of max -> freq, * fs / step to get proper values
-#             freq = np.argmax(abs(y_fourier)) * (fs / chunk_size)
-#             freqs.append(freq)
+    def butter_lowpass_filter(self, data, cutoff, fs, order=5):
+        b, a = self.butter_lowpass(cutoff, fs, order=order)
+        y = lfilter(b, a, data)
+        return y
 
-#         return freqs
+    def divide_signal_into_chunks(self, y, chunk_size):
+        return [y[i: i + chunk_size] for i in range(0, len(y), chunk_size)]
 
-# def get_score(path1, path2):
-#   CHUNK_SIZE = 4410
-#   LOW_CUT_FREQ = 600
+    def get_frequencies(self, y, fs, chunk_size):
+        freqs = []
+        for y_fourier in y:
+            y_fourier = fft(y_fourier)
+            y_fourier = y_fourier[0:int(chunk_size / 2)]
+            # index of max -> freq, * fs / step to get proper values
+            freq = np.argmax(abs(y_fourier)) * (fs / chunk_size)
+            freqs.append(freq)
 
-#   analyzer = VoiceAnalyzer()
+        return freqs
 
-#   fs, y = analyzer.read_signal(path1)
-#   fs2, y2 = analyzer.read_signal(path2)
+    def trim_frequencies(self, freq, freq2):
+        if(len(freq) > len(freq2)):
+            freq = freq[0:len(freq2)]
+        else:
+            freq2 = freq2[0:len(freq)]
+        return freq, freq2
 
-#   y = analyzer.butter_lowpass_filter(y, LOW_CUT_FREQ, fs)
-#   y2 = analyzer.butter_lowpass_filter(y2, LOW_CUT_FREQ, fs2)
 
-#   y = analyzer.divide_signal_into_chunks(y, CHUNK_SIZE)
-#   y2 = analyzer.divide_signal_into_chunks(y2, CHUNK_SIZE)
+class VoiceScoreCalculator:
+    def __init__(self):
+        self.CHUNK_SIZE = 4410
+        self.LOW_CUT_FREQ = 600
+        self.analyzer = VoiceAnalyzer()
 
-#   freq = analyzer.get_frequencies(y, CHUNK_SIZE)
-#   freq2 = analyzer.get_frequencies(y2, CHUNK_SIZE)
+    def get_mse(self, path1, path2):
+        fs, y = self.analyzer.read_signal(path1)
+        fs2, y2 = self.analyzer.read_signal(path2)
 
-#   if(len(freq) > len(freq2)):
-#       freq = freq[0:len(freq2)]
-#   else:
-#       freq2 = freq2[0:len(freq)]
+        y = self.analyzer.butter_lowpass_filter(y, self.LOW_CUT_FREQ, fs)
+        y2 = self.analyzer.butter_lowpass_filter(y2, self.LOW_CUT_FREQ, fs2)
 
-#   mse = np.square(np.subtract(freq, freq2)).mean()
+        y = self.analyzer.divide_signal_into_chunks(y, self.CHUNK_SIZE)
+        y2 = self.analyzer.divide_signal_into_chunks(y2, self.CHUNK_SIZE)
 
-#   return mse
+        freq = self.analyzer.get_frequencies(y, fs, self.CHUNK_SIZE)
+        freq2 = self.analyzer.get_frequencies(y2, fs2, self.CHUNK_SIZE)
+
+        freq, freq2 = self.analyzer.trim_frequencies(freq, freq2)
+        mse = np.square(np.subtract(freq, freq2)).mean()
+        return mse
+
+    def get_score(self, path1, path2):
+        mse = self.get_mse(path1, path2)
+
+        if(mse <= 1000):
+            return 100
+        elif(mse <= 10000):
+            return 50
+        elif(mse <= 50000):
+            return 25
+        else:
+            return 0
 
 
 def save_score_to_db():
@@ -101,18 +124,13 @@ def analysis(request):
             BASE_DIR, audio_track_vocal_path)
         upload_file_path = join_path_with_base_dir(BASE_DIR, upload_file_path)
 
-        # fs, y = wavfile.read(audio_track_vocal_path)
-        # print(fs)
-        # print(y)
+        # convert .webm file to .wav and save to the same dir
+        converted_file_path = convert_to_wav(upload_file_path)
 
-        # upload_file_path = upload_file_path.replace('/', '\\')
-
-        converted_file_wav = convert_to_wav(upload_file_path)
-
-        fs, y = wavfile.read(converted_file_wav)
-        print(fs)
-        print(y)
+        # Getting score for source and user record
+        voiceScoreCalculator = VoiceScoreCalculator()
+        score = voiceScoreCalculator.get_score(
+            audio_track_vocal_path, converted_file_path)
+        print(score)
 
     return render(request, 'landing-page/landing.html')
-
-    # save_score_to_db()

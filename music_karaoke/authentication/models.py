c65c58ce9e59fb5d58431b66f32c5299acec5103
puscik21 <grzesiekpuscizna@gmail.com>
2021-01-11 15:46:59 +0100

    Basic ranking page

--- music_karaoke/authentication/models.py
+++ music_karaoke/authentication/models.py
@@ -2,16 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 from songs.models import Song
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_pic = models.ImageField(blank= True, upload_to = "media/images")
+    profile_pic = models.ImageField(blank=True, upload_to="media/images")
+
 
 class UserFavouriteSong(models.Model):
-    id_user = models.ForeignKey(to=User, on_delete=models.CASCADE)
-    id_song = models.ForeignKey(to=Song, on_delete=models.CASCADE)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    song = models.ForeignKey(to=Song, on_delete=models.CASCADE)
+
 
 class Ranking(models.Model):
-    id_user = models.ForeignKey(to=User, on_delete=models.CASCADE)
-    id_song = models.ForeignKey(to=Song, on_delete=models.CASCADE)
+    profile = models.ForeignKey(to=Profile, on_delete=models.CASCADE)
+    song = models.ForeignKey(to=Song, on_delete=models.CASCADE)
     score = models.IntegerField()
     datetime = models.DateTimeField()
--- music_karaoke/songs/views.py
+++ music_karaoke/songs/views.py
@@ -1,25 +1,48 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required, permission_required
 from .forms import SongForm
 from .models import Song
+# from ..authentication.models import Ranking, Profile
+from authentication.models import Ranking, Profile
+
 
 def landing(request):
     return render(request, 'landing-page/landing.html')
 
+
 # Create your views here.
 def index(request):
     songs = Song.objects.all()
-    return render(request, 'song_index.html', {'songs' : songs})
+    return render(request, 'song_index.html', {'songs': songs})
+
 
 def show_details(request, song_id):
     song = Song.objects.get(id=song_id)
-    return render(request, 'details.html', {'song' : song})
+    return render(request, 'details.html', {'song': song})
+
 
 def add_song_to_favourites(request, song_id):
     pass
 
+
 def show_ranking(request):
-    pass
+    ranking = Ranking.objects.order_by('score').reverse().all()
+    position_count = 0
+    for rank in ranking:
+        profile = rank.profile
+        user = profile.user
+        rank.first_name = user.first_name
+        rank.last_name = user.last_name
+        rank.profile_pic = profile.profile_pic
+
+        song = rank.song
+        rank.song = song
+
+        position_count += 1
+        rank.position_count = position_count
+    return render(request, 'ranking.html', {'ranking': ranking})
+
 
 @permission_required('is_superuser', login_url='/')
 def add_new_song(request):

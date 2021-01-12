ef3285974447ae5c6c625ec8f0361767e1b5b085
heIIoad <adrian.stepnik1997@gmail.com>
2021-01-12 11:18:15 +0100

    Adding songs to favourites feature

--- music_karaoke/songs/urls.py
+++ music_karaoke/songs/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('/<int:song_id>', views.show_details, name="show_details"),
     # songs/AddSong
     path('/AddSong', views.add_new_song, name="add_new_song"),
-    # songs/add_to_favourite
-    path('/add_to_favourite', views.add_song_to_favourites, name="add_to_favourite"),
+    # songs/<id>/add_to_favourite
+    path('/<int:song_id>/add_to_favourite', views.add_song_to_favourites, name="add_to_favourite"),
+    # songs/favourites
+    path('/favourites', views.show_favourites, name="show_favourites"),
 ]

711c17b6af1e4c49c21bf8ba80da9c9fd8c0438f
heIIoad <adrian.stepnik1997@gmail.com>
2021-01-19 14:50:57 +0100

    Correctly filtering favourite songs based on user

--- music_karaoke/songs/views.py
+++ music_karaoke/songs/views.py
@@ -17,7 +17,7 @@ def index(request):
 
 def show_details(request, song_id):
     song = Song.objects.get(id=song_id)
-    favourite = UserFavouriteSong.objects.filter(song = song).count()
+    favourite = UserFavouriteSong.objects.filter(song = song, user = request.user).count()
     return render(request, 'details.html', {'song': song, 'favourite':favourite})
 
 def show_favourites(request):
@@ -36,7 +36,7 @@ def add_song_to_favourites(request, song_id):
 
 def remove_song_from_favourites(request, song_id):
     song_to_delete = Song.objects.get(id=song_id)
-    UserFavouriteSong.objects.filter(song=song_to_delete).delete()
+    UserFavouriteSong.objects.filter(song=song_to_delete, user=request.user).delete()
     return redirect('../{}'.format(song_id))
 
 @permission_required('is_superuser', login_url='/')

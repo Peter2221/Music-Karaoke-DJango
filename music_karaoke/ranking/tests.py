d2f270f75d137b5ed8d07f4d775915ab76dfc6ac
Peter2221 <pmbasen@gmail.com>
2021-01-28 21:50:45 +0100

    Add test for ranking

--- music_karaoke/ranking/tests.py
+++ music_karaoke/ranking/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from authentication.models import Profile
+from .views import save_in_database
 
-# Create your tests here.
+
+class RankingTest(TestCase):
+    def test_show_ranking(self):
+        response = self.client.get(reverse('ranking:index'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'ranking.html')
